e4382240385e859213900eeb9aa6b6af0e40998b
Anshika-Singla <anshikasingla14.as@gmail.com>
2026-03-29 11:11:32 +0530

    fix cors

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ Question: ${question}`
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
